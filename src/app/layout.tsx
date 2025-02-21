5b4923e8bb70a12f907d3395ea5201ad5ce037af
williamwu277 <williamwu277@gmail.com>
2025-02-21 15:36:48 -0500

    Fix transition horizontal scroll-bar bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
         <Providers>
           {children}
